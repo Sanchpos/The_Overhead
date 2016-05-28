d0338a9bdfddf5faec14fd28516c35868994fa56
Vadim Chetkov <vadim.chetkov@gmail.com>
2016-05-28 14:40:43 +0500

    Добавил случайные эффекты

--- Assets/The Overhead Assets/Scripts/Footsteps.cs
+++ Assets/The Overhead Assets/Scripts/Footsteps.cs
@@ -11,7 +11,9 @@ public class Footsteps : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (cmc.isGrounded() && cmc.getSpeed() > 0 && !GetComponent<AudioSource>().isPlaying) {
+		if (cmc.isGrounded() && cmc.getSpeed() != 0 && !GetComponent<AudioSource>().isPlaying) {
+			GetComponent<AudioSource>().volume = Random.Range (0.8f, 1);
+			GetComponent<AudioSource>().pitch = Random.Range (0.8f, 1.2f);
 			GetComponent<AudioSource>().Play();
 		}
 	}
